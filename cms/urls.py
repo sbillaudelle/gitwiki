c70153d973017296655b6c144fc1828cfcd69bca
Cream Desktop Environment <github@cream-project.org>
2010-03-14 01:20:47 +0100

    Fixed update-URL issue.

--- cms/urls.py
+++ cms/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'^update', 'cms.updater.update'),
     (r'^(?P<path>.+)/((?P<revision>[0-9a-f]{40}))', 'cms.views.view'),
     (r'^(?P<path>.*)', 'cms.views.view'),
-    (r'^update', 'cms.updater.update'),
     (r'', 'django.views.defaults.page_not_found')
 )
